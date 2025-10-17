5726fe573c06981598cb20436f627df386e5e6cd
zinine73 <zinine@naver.com>
2025-10-17 15:50:06 +0900

    enemy

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public SpawnManager sm;
+    public GameObject powerupIndicator;
+    public bool hasPowerup;
     public float speed = 5.0f;
+    float powerupStrength = 15.0f;
     Rigidbody playerRb;
     GameObject focalPoint;
 
@@ -20,5 +24,50 @@ public class PlayerController : MonoBehaviour
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward
             * speed * forwardInput);
+        powerupIndicator.transform.position
+            = transform.position
+            + new Vector3(0, -0.5f, 0);
+        if (transform.position.y < -10)
+        {
+            sm.isGameOver = true;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("POWERUP"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdown());
+            powerupIndicator.SetActive(true);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("ENEMY")
+            && hasPowerup)
+        {
+            Rigidbody enemyRb = collision.gameObject
+                .GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer =
+                (collision.gameObject.transform.position
+                 - transform.position);
+
+            Debug.Log("Collided with "
+                + collision.gameObject.name
+                + " with powerup set to "
+                + hasPowerup);
+            enemyRb.AddForce(awayFromPlayer * powerupStrength,
+                ForceMode.Impulse);
+        }
+    }
+
+    IEnumerator PowerupCountdown()
+    {
+        yield return new WaitForSeconds(7f);
+        hasPowerup = false;
+        powerupIndicator.SetActive(false);
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,9 +5,48 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
+    public int enemyCount;
+    public int waveNumber = 1;
+    float spawnRange = 9f;
+    public bool isGameOver = false;
     void Start()
     {
-        Instantiate(enemyPrefab, new Vector3(0, 0, 6),
-            enemyPrefab.transform.rotation);
+        NextWave();
+    }
+
+    void SpawnEnemyWave(int enemisToSpawn)
+    {
+        for (int i = 0; i < enemisToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(),
+                enemyPrefab.transform.rotation);
+        }
+    }
+
+    Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        return randomPos;
+    }
+
+    void Update()
+    {
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if (enemyCount == 0)
+        {
+            waveNumber++;
+            NextWave();
+        }
+    }
+    
+    void NextWave()
+    {
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powerupPrefab,
+            GenerateSpawnPosition(),
+            powerupPrefab.transform.rotation);
     }
 }
